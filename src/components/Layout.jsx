c665c9ca913fb12af7f93e474d2702550191ac0c
marilianewton <mariliagomesnewton@gmail.com>
2025-04-14 12:36:47 +0100

    Bg animation scroll triggered

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,16 +2,25 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import { useRef } from "react";
+import { useInView } from "motion/react";
+import { motion } from "motion/react";
 
 export default function Layout() {
   const portfolioRef = useRef(null);
+  const isBgBlack = useInView(portfolioRef, { amount: 0.1 });
   return (
     <>
       {/* {loading && <Loader />} */}
       <Header />
-      <div className="wrapper">
+      <motion.div
+        style={{
+          backgroundColor: isBgBlack ? "#000000" : "#fff",
+          transition: "background-color 2s ease-in-out",
+        }}
+        className="wrapper"
+      >
         <Outlet context={{ portfolioRef }} />
-      </div>
+      </motion.div>
       {/* <Footer /> */}
     </>
   );
